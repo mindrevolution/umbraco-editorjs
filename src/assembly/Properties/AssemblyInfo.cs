7968c8a7895d0c92324dfdb41bb78b2761a42840
Marc <marc.stoecker@mindrevolution.com>
2019-05-31 01:27:07 +0200

    Add bin for easier testing of the prototype until we have a package

--- src/assembly/Properties/AssemblyInfo.cs
+++ src/assembly/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("assembly")]
+[assembly: AssemblyTitle("Editor.js for Umbraco")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("assembly")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyProduct("Editor.js for Umbraco")]
+[assembly: AssemblyCopyright("Copyright ©  2019 mindrevolution")]
+[assembly: AssemblyTrademark("mindrevolution is a registered trademark")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
